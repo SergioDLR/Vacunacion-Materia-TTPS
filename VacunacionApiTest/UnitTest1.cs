fafc7b27f3360e2f7f6e83a946bc22db31f523b7
salo2019 <jorgesalo86@gmail.com>
2022-12-17 21:48:56 -0300

    pruebas unix

--- VacunacionApiTest/UnitTest1.cs
+++ VacunacionApiTest/UnitTest1.cs
@@ -1,11 +1,79 @@
+using DocumentFormat.OpenXml.InkML;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Json;
+using System.Text.Json;
+using VacunacionApi.Controllers;
+using VacunacionApi.DTO;
+using VacunacionApi.Models;
+
 namespace VacunacionApiTest
 {
-    public class UnitTest1
+    public class UnitTest1 : ControllerBase
     {
-        [Fact]
-        public void Test1()
+        private readonly VacunasContext _context;
+        private readonly UsuariosController _usuariosController;
+
+        private readonly RequestUsuarioDTO _usuarioDTO;
+        private readonly ResponseUsuarioDTO ResponseUsuarioDTO;
+        //private readonly VacunasContext _vacunasContext;    
+        private readonly ResponseListaUsuariosDTO _listaUsuariosDTO;
+
+
+        public UnitTest1()
         {
 
+            _usuariosController = new UsuariosController(_context);
+
+            //_listaUsuariosDTO = new ResponseListaUsuariosDTO();
+            
+            _usuarioDTO = new RequestUsuarioDTO();
+            //ResponseUsuarioDTO = new ResponseUsuarioDTO();
+            //_listaUsuariosDTO = null;
+        }
+
+        [Fact]
+        public async void Test1()
+        {
+            //model del request
+            _usuarioDTO.EmailAdministrador = "fabi@gmail.com";
+            _usuarioDTO.Email = "operadorNacionalTest@hotmail.com";
+            _usuarioDTO.Password = "123456";
+            _usuarioDTO.IdJurisdiccion = 1;
+            _usuarioDTO.IdRol = 3;
+
+
+
+            await _usuariosController.CrearUsuario(_usuarioDTO);
+
+            //            var result = await _usuariosController.CrearUsuario(_usuarioDTO);
+            var result2 = await _usuariosController.GetAll("fabi@gmail.com", 1, 1);
+            var estado = result2.Value.EstadoTransaccion;
+
+            //Assert.IsType<OkObjectResult>(codigoHttp);
+
+
+            //Assert.Equal("fabi@gmail.com", "fabi@gmail.com");
+
+            //response
+            //ResponseUsuarioDTO.EmailAdministrador = "fabi@gmail.com";
+            //ResponseUsuarioDTO.ExistenciaErrores = false;
+            //ResponseUsuarioDTO.EstadoTransaccion = "Aceptada";
+            //ResponseUsuarioDTO.UsuarioDTO = new UsuarioDTO(0, _usuarioDTO.Email, _usuarioDTO.Password, _usuarioDTO.IdJurisdiccion, _usuarioDTO.IdRol, "Buenos Aires", "Operador Nacional");
+            //Assert.Equal("fabi@gmail.com", ResponseUsuarioDTO.EmailAdministrador);
+
+
+            //ResponseUsuarioDTO response = result.Value.Errores;
+
+            Assert.True(estado.Equals(true));   
+    
+            //Assert.IsType<ViewResult>(result2);
+
+
+
+            //Assert.True(result.IsCompletedSuccessfully);
+            //Assert.IsType<OkObjectResult>(result);
+            //Assert.False(condition: result.Value.ExistenciaErrores);
         }
     }
 }
