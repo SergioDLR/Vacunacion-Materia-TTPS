2034393868420f1af9c9e03dab53ca98108db1c5
Sergio López Rey <43255668+SergioDlr@users.noreply.github.com>
2022-12-19 13:45:41 -0300

    -

--- front-app/src/components/AnalistaProvincial/VacunadosInfo/Vacunados.jsx
+++ front-app/src/components/AnalistaProvincial/VacunadosInfo/Vacunados.jsx
@@ -6,28 +6,75 @@ import VacunadosTable from "./VacunadosTable";
 import { Container } from "@mui/system";
 import CustomLoader from "@/components/utils/CustomLoader";
 import { useAlert } from "react-alert";
+import { InputLabel, MenuItem, Pagination, Select } from "@mui/material";
 const Vacunados = () => {
   const { userSesion } = useContext(UserContext);
   const [vacunados, setVacunados] = useState([]);
   const [estaCargando, setEstaCargando] = useState(true);
+  const [cantPages, setCantPages] = useState(0);
+  const [cantidadElementos, setCantidadElementos] = useState(10);
   const alert = useAlert();
   useEffect(() => {
     try {
-      axios
-        .get(`${allUrls.vacunasAplidas}?emailUsuario=${userSesion.email}`)
-        .then((response) => {
-          setEstaCargando(false);
-          setVacunados(response?.data?.listaVacunasAplicadasDTO);
-        })
-        .catch((e) => console.log(e));
+      loadData(1, cantidadElementos);
     } catch (e) {
       alert.error(`Ocurrio un error del lado del servidor`);
     }
   }, []);
+
+  const handleChangePage = (evt, value) => {
+    setEstaCargando(true);
+    setPage(value);
+    console.log(value);
+    loadData(value * cantidadElementos - cantidadElementos, cantidadElementos);
+  };
+
+  const calculatePages = (value = 10) => {
+    if (userSesion.descripcionJurisdiccion === "Nación")
+      axios.get(allUrls.cantResultados).then((response) => setCantPages(parseInt(response.data / value) + 1));
+    else
+      axios
+        .get(`${allUrls.cantResultados}?descripcionJurisdiccion=${userSesion.descripcionJurisdiccion}`)
+        .then((response) => setCantPages(parseInt(response.data / value) + 1));
+  };
+  useEffect(() => {
+    calculatePages();
+  }, []);
+
+  const loadData = (inicial, final) => {
+    axios
+      .get(`${allUrls.vacunasAplidas}?emailUsuario=${userSesion.email}&skip=${inicial}&take=${final}`)
+      .then((response) => {
+        setEstaCargando(false);
+        setVacunados(response?.data?.listaVacunasAplicadasDTO);
+      })
+      .catch((e) => console.log(e));
+  };
+  const handleChangeQuantity = (evt) => {
+    setCantidadElementos(evt.target.value);
+    loadData(1, evt.target.value);
+    calculatePages(evt.target.value);
+  };
   return (
     <Container>
       {estaCargando && <CustomLoader />}
       <VacunadosTable vacunados={vacunados} />
+      <Pagination count={cantPages} page={page} onChange={handleChangePage} siblingCount={5} />
+      <InputLabel id="demo-simple-select-label">Cantidad de elementos por pagina</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={cantidadElementos}
+        label="Cantidad de elementos por pagina"
+        onChange={handleChangeQuantity}
+      >
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+        <MenuItem value={30}>30</MenuItem>
+        <MenuItem value={40}>40</MenuItem>
+        <MenuItem value={50}>50</MenuItem>
+        <MenuItem value={60}>60</MenuItem>
+      </Select>
     </Container>
   );
 };
