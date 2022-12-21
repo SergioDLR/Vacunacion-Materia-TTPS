17bd78abdf7b54534db2e15de9a70bf96baa81a9
salo2019 <jorgesalo86@gmail.com>
2022-12-21 10:50:16 -0300

    se agrega aplicacion Test xUnit

---
+++ TestVacunacionApi/UnitTest1.cs
@@ -0,0 +1,42 @@
+using VacunacionApi.DTO;
+using VacunacionApi.Testing;
+
+namespace TestVacunacionApi
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void TestValidarEmailNuevoUsuario()
+        {
+            //ingreso de nuevo usuario
+            var usuarioDTO = new UsuarioDTO(2, "operadorNacional3@gmail.com", "12345", 3, 24, "Nacion", "Operador Nacional");
+
+            //Arrange
+            var mailValidator = new UsuariosTest();
+            var emailAddress = usuarioDTO.Email;
+
+            //Act
+            bool isValid = mailValidator.isValidMail(emailAddress);
+
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("mariaAdministradora@gmail.com", true)]
+        [InlineData("fernandoAdministrador@gmail.com", true)]
+        [InlineData("operador@gmail.com", false)]
+        [InlineData("otroEmail@gmail.com", false)]
+        public void ValidarEmailAdministrador(string emailAddress, bool expected)
+        {
+            //Arrange
+            var mailValidator = new UsuariosTest();
+
+            //Act
+            bool isValid = mailValidator.isAdminEmail(emailAddress);
+
+            //Assert
+            Assert.Equal(isValid, expected);
+        }
+    }
+}
---
+++ back-app/Testing/UsuariosTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VacunacionApi.Testing
+{
+    public class UsuariosTest
+    {
+        public bool isValidMail(string emailAddress)
+        {
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                throw new EmailNoRecibidoException();
+            }
+            Regex regex = new Regex(@"^[\w0-9._%+-]+@[\w0-9.-]+\.[\w]{2,6}$");
+            return regex.IsMatch(emailAddress);
+        }
+        public string isExistEmail(string emailAddress)
+        {
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                throw new EmailNoRecibidoException();
+            }
+            List<string> listEmailsExistentes = new List<string>() { "mariaOperadorNacional@gmail.com", "fernandoAnalistaBuenosAires@gmail.com" };
+            return listEmailsExistentes.Any(d => emailAddress == d) ? "Email ya existente" : "Email creado correctamente";
+        }
+
+        public bool isAdminEmail(string emailAddress)
+        {
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                throw new EmailNoRecibidoException();
+            }
+            List<string> listEmailsAdmins = new List<string>() { "mariaAdministradora@gmail.com", "fernandoAdministrador@gmail.com" };
+            //return listEmailsExistentes.Any(d => emailAddress.Contains) ? "Email ya existente" : "Email creado correctamente";
+            return listEmailsAdmins.Contains(emailAddress);
+        }
+    }
+}
